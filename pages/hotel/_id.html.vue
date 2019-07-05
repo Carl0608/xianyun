b22bea84c880e161548ac1adad66ddfe176dc3e2
orangestar <247434289@qq.com>
2019-07-05 09:48:55 +0800

    测试一下

---
+++ pages/hotel/_id.html.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>动态路由</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const { id } = this.$route.params;
+    this.$axios({
+      url: `hotels/${id}`,
+      method:"get"
+    });
+  },
+  data() {
+    return {
+      id: ""
+    };
+  },
+  method: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
